ffdf650f56fffa3b306d5a32fd90213986e478c2
sh6khan <sadman.khan999@gmail.com>
2015-02-08 23:49:38 -0500

    explaining the int of listen

--- adding_listener.rb
+++ adding_listener.rb
@@ -6,4 +6,6 @@ addr = Socket.pack_sockaddr_in(4481,'0.0.0.0')
 server_socket.bind(addr)
 
 #listen for incoming connections
+# the number represents the maximum number of pending connections the server socket 
+# is willing to tolerate 
 server_socket.listen(5)
