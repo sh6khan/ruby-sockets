58f04c9fcf52c68ef24c7be6b90529722af7b10a
sh6khan <sadman.khan999@gmail.com>
2015-02-08 23:37:31 -0500

    ip address binding examples

---
+++ ip_address_binding.rb
@@ -0,0 +1,17 @@
+require 'socket'
+
+#only listen for clients from localhost
+local_socket = Socket.new(:INET, :STREAM)
+local_addr = Socker.pack_sockaddr_in(4481, '127.0.0.1')
+local_socket.bind(local_addr)
+
+#this socket will bind to anything and listen to any clients
+any_socket = Socket.new(:INET, :STREAM)
+any_addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+any_socket.bind(any_addr)
+
+#this socket attempts to bind to an unkown interface
+#therefore it will raise the Errno::EADDRNOTAVAIL
+error_socket = Socket.new(:INET, :STREAM)
+error_addr = Socket.pack_sockaddr_in(4481, '1.2.3.4')
+error_socket.bind(error_addr)
